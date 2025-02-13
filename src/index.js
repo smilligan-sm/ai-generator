92558f599f7743d3658432c841068d1788eeb578
smilligan-sm <sarahlouisemilligan.sm@gmail.com>
2025-02-13 16:29:44 +1100

    added API and made some tweaks to CSS to tidy things up

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
+function displayFact(response) {
+  new Typewriter("#results", {
+    strings: response.data.answer,
+    autoStart: true,
+    cursor: "",
+    delay: 10,
+  });
+}
+
 function generateFact(event) {
   event.preventDefault();
 
-  new Typewriter("#results", {
-    strings:
-      "A German shepherd named Talero stayed next to the body of his owner for 23 days after he had died in a snow storm. He prevented animals from attacking the body and tried to keep his owner warm by sleeping by his side.",
-    autoStart: true,
-    cursor: null,
-    delay: 20,
-  });
+  let userInput = document.querySelector("#user-instructions");
+  let apiKey = "24f16ff06b6aba2369ec3846f0t8bco2";
+  let prompt = `You are a smart AI assistant who likes to tell us weird but true facts. Your mission is to tell us a fact that directly relates to the topic of the User Instructions. Your result must be 2 sentences long and in basic HTML. Please add a <br /> to the end of the first sentence, remove any empty trailing spaces and do not add any markdown to the result.`;
+  let context = `User Instructions: tell me a fact about ${userInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating Fact");
+  console.log(`Context: ${context}`);
+  console.log(`Prompt: ${prompt}`);
+
+  axios.get(apiUrl).then(displayFact);
 }
 
 let factFormElement = document.querySelector("#form-generator");
