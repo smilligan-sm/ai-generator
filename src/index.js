5d0a4a23a91fa7db146f71c7ed42915c3fbb0b44
smilligan-sm <sarahlouisemilligan.sm@gmail.com>
2025-02-13 16:34:26 +1100

    Minor update to the API prompt call

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ function generateFact(event) {
 
   let userInput = document.querySelector("#user-instructions");
   let apiKey = "24f16ff06b6aba2369ec3846f0t8bco2";
-  let prompt = `You are a smart AI assistant who likes to tell us weird but true facts. Your mission is to tell us a fact that directly relates to the topic of the User Instructions. Your result must be 2 sentences long and in basic HTML. Please add a <br /> to the end of the first sentence, remove any empty trailing spaces and do not add any markdown to the result.`;
+  let prompt = `You are a smart AI assistant who likes to tell us weird but true facts. Your mission is to tell us a fact that directly relates to the topic of the User Instructions. Your result must be 2 sentences long and in basic HTML. Please add a <br /> to the end of the first sentence and do not add any markdown to the result.`;
   let context = `User Instructions: tell me a fact about ${userInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
